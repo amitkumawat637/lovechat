93664dcb76b76dc511844c319d5a06930c1862f8
amitkumawat637 <amitak2550@gmail.com>
2026-08-05 09:22:13 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, getUsers,loginUser  } = require("../controllers/userController");
+const {
+  registerUser,
+  getUsers,
+  getUserById,
+  updateUser,
+  loginUser,
+} = require("../controllers/userController");
 const upload = require("../middleware/upload");
 
 router.post("/register", upload.single("photo"), registerUser);
 router.get("/", getUsers);
+router.get("/:id", getUserById);
+router.put("/:id", upload.single("photo"), updateUser);
 router.post("/login", loginUser);
 
 module.exports = router;
